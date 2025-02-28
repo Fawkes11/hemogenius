0c1ff9f0d77f68f177fdaaff993701b71b863d6f
Fawkes11 <hesslerinampuez@gmail.com>
2025-02-27 22:48:33 -0500

    Desactivar el nombre copyright

--- src/app/components/SVGcomponents/CardsInfo.js
+++ src/app/components/SVGcomponents/CardsInfo.js
@@ -17,12 +17,12 @@ const CardsInfo = ({ cardInfo }) => {
                 getCardInfoComponent(cardInfo)
             }
 
-            <text
+            {/* <text
                 transform='translate(435.35 265) rotate(-90)'
                 className={patrickHand.className}
                 fontFamily="Patrick Hand"
                 fontSize="11"
-            ><tspan>© Sofia Castillo</tspan></text>
+            ><tspan>© Sofia Castillo</tspan></text> */}
         </>
     )
 }
@@ -90,41 +90,41 @@ const getCardInfoComponent = (cardInfo) => {
                     ><tspan>Monoclonal</tspan></text>
                 </g>
             );
-            case "card_05":
-                return (
-                    <g id="cardInfo">
-                        <line x1="16.259" y1="214.502" x2="288.159" y2="214.502" fill="none" stroke='#000000' strokeMiterlimit="10" strokeWidth="1" />
-                        <line x1="288.159" y1="201.002" x2="288.159" y2="215.002" fill="none" stroke='#000000' strokeMiterlimit="10" strokeWidth="1" />
-                        <rect x="288.7" y="201.002" width="68" height="14" fill="#ffd271" />
-                        <text
-                            transform='translate(151 210.8)'
-                            className={patrickHand.className}
-                            fontFamily="Patrick Hand"
-                            textAnchor='middle'
-                            fontSize="10"
-                        ><tspan>rabbit</tspan></text>
-                        <text
-                            transform='translate(322.5 210.8)'
-                            className={patrickHand.className}
-                            fontFamily="Patrick Hand"
-                            textAnchor='middle'
-                            fontSize="10"
-                        ><tspan>Monoclonal</tspan></text>
-                    </g>
-                );
+        case "card_05":
+            return (
+                <g id="cardInfo">
+                    <line x1="16.259" y1="214.502" x2="288.159" y2="214.502" fill="none" stroke='#000000' strokeMiterlimit="10" strokeWidth="1" />
+                    <line x1="288.159" y1="201.002" x2="288.159" y2="215.002" fill="none" stroke='#000000' strokeMiterlimit="10" strokeWidth="1" />
+                    <rect x="288.7" y="201.002" width="68" height="14" fill="#ffd271" />
+                    <text
+                        transform='translate(151 210.8)'
+                        className={patrickHand.className}
+                        fontFamily="Patrick Hand"
+                        textAnchor='middle'
+                        fontSize="10"
+                    ><tspan>rabbit</tspan></text>
+                    <text
+                        transform='translate(322.5 210.8)'
+                        className={patrickHand.className}
+                        fontFamily="Patrick Hand"
+                        textAnchor='middle'
+                        fontSize="10"
+                    ><tspan>Monoclonal</tspan></text>
+                </g>
+            );
 
-                case "card_06":
-                    return (
-                        <g id="cardInfo">
-                            <line x1="16.259" y1="214.502" x2="424.259" y2="214.502" fill="none" stroke='#000000' strokeMiterlimit="10" strokeWidth="1" />
-                            <text
-                                transform='translate(219.5 210.6)'
-                                className={patrickHand.className}
-                                fontFamily="Patrick Hand"
-                                textAnchor='middle'
-                                fontSize="10"
-                            ><tspan>Anti-IgG rabbit</tspan></text>
-                        </g>
-                    );
+        case "card_06":
+            return (
+                <g id="cardInfo">
+                    <line x1="16.259" y1="214.502" x2="424.259" y2="214.502" fill="none" stroke='#000000' strokeMiterlimit="10" strokeWidth="1" />
+                    <text
+                        transform='translate(219.5 210.6)'
+                        className={patrickHand.className}
+                        fontFamily="Patrick Hand"
+                        textAnchor='middle'
+                        fontSize="10"
+                    ><tspan>Anti-IgG rabbit</tspan></text>
+                </g>
+            );
     }
 }
