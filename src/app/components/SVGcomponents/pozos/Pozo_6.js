1990bb7d640b65cbacd896864452cadd6e158ac8
Hessler Inampuez <hesslerinampuez@gmail.com>
2026-06-05 11:29:15 -0500

    update plasma position in pozo 6

--- src/app/components/SVGcomponents/pozos/Pozo_6.js
+++ src/app/components/SVGcomponents/pozos/Pozo_6.js
@@ -8,9 +8,9 @@ const Pozo_6 = ({ rowData }) => {
     return (
         <g id="pozo_x5F_6_x5F_ag">
             { GradoComponent }
-            { rbc &&  <rect x="367.619" y="14.752" width="45.48" height="10" fill="#ffb6c0" /> }
+            { plasma &&  <rect x="367.619" y="14.752" width="45.48" height="10" fill="#f1ee9e" /> }
             { enzimas &&  <rect x="367.619" y="24.752" width="45.48" height="10" fill="#0085bf" /> }
-            { plasma &&  <rect x="367.619" y="34.752" width="45.48" height="10" fill="#f1ee9e" /> }
+            { rbc && <rect x="367.619" y="34.752" width="45.48" height="10" fill="#ffb6c0" /> }
         </g>
     )
 }
